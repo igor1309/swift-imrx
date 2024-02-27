c2562bd8573b5ef5082b63942d6d42db42a5df5b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-02-27 07:09:00 +0300

    Create `IMRx` project

---
+++ App/IMRx/IMRxApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMRxApp.swift
+//  IMRx
+//
+//  Created by Igor Malyarov on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct IMRxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
